ab57b668b2d592a8de9100f21106590d9dd00377
julia-015 <jumd.vecchia@gmail.com>
2024-05-23 14:09:50 -0300

    Instalação e criação de projetos
    Co-authored-by: Anjosisa <Anjosisa@users.noreply.github.com>
    Co-authored-by: muriloxz77 <muriloxz77@users.noreply.github.com>
    Co-authored-by: Giulia Rocha <giuliarocha04@users.noreply.github.com>

---
+++ AgendamentoSalas/app/index.tsx
@@ -0,0 +1,11 @@
+import LoginTab from './login';
+
+const App = () => {
+    return (
+        <div>
+            <LoginTab />
+        </div>
+    );
+};
+
+export default App;
---
+++ AgendamentoSalas/app/login.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const LoginTab = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const log = () => {
+        
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Login</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Username"
+                value={username}
+                onChangeText={setUsername}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+            />
+            <Button title="Login" onPress={log} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 16,
+    },
+    header: {
+        fontSize: 24,
+        marginBottom: 16,
+        textAlign: 'center',
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 12,
+        padding: 10,
+    },
+});
+
+export default LoginTab;
